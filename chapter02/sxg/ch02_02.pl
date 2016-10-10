dbf766f9d1b90d9fa52368338d3a8f98e26d1a1c
sebing <sebingeo@yahoo.com>
2016-10-10 19:22:00 +0100

    Review changes

--- chapter02/sxg/ch02_02.pl
+++ chapter02/sxg/ch02_02.pl
@@ -7,12 +7,7 @@ use Module::CoreList;
 
 my @modules =  $Module::CoreList::version { 5.014002 };
 
-my $max_length = 0;
-foreach my $module (@modules) {
-	if (length $module > $max_length){
-		$max_length = length $module
-	}
-}
+
 foreach my $mod (@modules) {
-	print "$mod : ". Module::CoreList->first_release( $mod )."\n";
+	print "$mod : ". $Module::CoreList::released(Module::CoreList->first_release( $mod ))."\n";
 }
