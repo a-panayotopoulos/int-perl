2e182e1122b7102201c6900d6a607259fc57b0cb
Mark Wignall <mark.wignall@datacash.com>
2016-10-31 09:17:51 +0000

    Added chapter 5 stuff

---
+++ chapter05/mdw/Ex1.pl
@@ -0,0 +1,6 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+print "Whoever wrote this mixed up hashes and hash references as they declare the passengers are hashes but by using curly brakets not parentisies(sp?) they assign the values as hash references.\n";
---
+++ chapter05/mdw/Ex3.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my %traffic;
+
+foreach ( <> ){
+	my $imput = $_;
+	if ( $imput =~ /#/ ) {
+		next;
+	}
+	my @line = split(/\t/, $imput );
+	$traffic{$line[0]}{$line[1]} += $line[2];
+}
+
+my %totals;
+my @senders = keys %traffic;
+
+foreach my $sender ( @senders ) {
+	$totals{$sender} =  size_sender( $sender );
+}
+
+@senders = sort { $totals{$b} <=> $totals{$a} } @senders;
+
+my $length = @senders - 1;
+
+foreach my $i ( 0..$length){
+	print "$senders[$i]\n";
+	#do recivers
+	my @recivers = keys $traffic{$senders[$i]};
+	@recivers = sort { $traffic{$senders[$i]}{$b} <=> $traffic{$senders[$i]}{$a} } @recivers;
+	my $reciver_length  = @recivers - 1;
+	foreach my $y ( 0..$reciver_length){
+		print "\t$recivers[$y] $traffic{$senders[$i]}{$recivers[$y]}\n";
+	}
+}
+
+sub size_sender {
+	my $sender = shift;
+	my $size;
+	foreach my $reciver ( keys %{$traffic{$sender}} ) {
+        	$size += $traffic{$sender}{$reciver};
+	}
+	return $size;
+}
+
+
