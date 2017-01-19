ab0969ba2552d6b7642da144e4d191115037ad71
axp <alex_panayotopoulos@mastercard.com>
2017-01-19 16:34:30 +0000

    Add handy git hints

---
+++ chapter14/axp/ex01b.ps1
@@ -0,0 +1,2 @@
+#!/bin/bash
+git diff -w 18c46df903aa5c41ccdc8fc4a75131fc2ff57c4c 070f7724e700b8fea15a22c33fd2cf4b6cb3c7d5
---
+++ chapter14/axp/ex05.ps1
@@ -0,0 +1,2 @@
+#!/bin/bash
+git show b5b01b5fae97c80d61968002c409bbcc4c7a7c1a
