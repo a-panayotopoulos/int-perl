8640a230b69089b0e8c4d29a7e8dff4cfe362da6
unknown <e047024@.corp.mastercard.org>
2017-02-20 10:41:17 +0000

    Updated for chapter17

---
+++ modules/slc/chapter17/ex2.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Oogaboogoo::Date qw( :all );
+
+my($sec, $min, $hour, $mday, $mon, $year, $wday) = localtime;
+
+my $day_name = day($wday);
+my $mon_name = mon($mon);
+$year += 1900;
+
+print "Today is $day_name, $mon_name $mday, $year.\n";
+
